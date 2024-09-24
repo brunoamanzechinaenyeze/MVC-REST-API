d6fc6fbe629b54f7703d67522b4bfab5f62d59ee
Amanze Bruno <heroicemblem69@gmail.com>
2024-09-24 11:46:10 +0100

    Done with the Model import

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -4,12 +4,12 @@ const path = require('path');
 const employeesController = require('../../controllers/employeesController')
 
 router.route('/')
-    .get()
-    .post()
-    .put()
-    .delete();
+    .get(employeesController.getAllEmployees)
+    .post(employeesController.createNewEmployee)
+    .put(employeesController.updateEmployee)
+    .delete(employeesController.deleteEmployee);
 
 router.route('/:id')
-    .get()
+    .get(employeesController.getEmployee)
 
 module.exports = router;
